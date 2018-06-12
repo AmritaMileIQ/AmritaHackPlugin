2d75a6d49f6e9a78b40036362731e9dbf75cf358
AmritaMileIQ <amchowdh@microsoft.com>
2018-06-12 12:05:28 -0700

    post expense

---
+++ post-expenses.js
@@ -0,0 +1,32 @@
+function create_expenses(user_parse_id, amount, classify, name, date, notes, callback){
+	$(document).ready(function() {
+	  var expense = {
+  		"amount": amount,
+  		"classification": classify,
+  		"merchant_name": name,
+  		"expense_date": date,
+  		"notes": notes
+	    }
+	    alert(JSON.stringify(expense))
+      $.ajax({
+      		crossDomain: true,
+      		dataType: 'json',
+	        url: 'https://spend-staging.mobiledatalabs.com/api/1/expenses/',
+	      	headers: {
+	        'X-MileIQ-Application-Id':'E7EC264F-F470-4C7C-95AF-4B5D08C57346',
+	        'X-MileIQ-API-Key':'1BD272D1-3DC4-4AF4-BCC6-D125C521788C',
+	        'X-MileIQ-Acting-On': user_parse_id,
+	        'Content-Type':'application/json'
+	    	},
+	      	type: 'POST',
+	      	data: JSON.stringify(expense),
+	      	success: function(response) {
+	        alert(JSON.stringify(response))
+	        callback(response);
+	      	},
+	      	error: function() {
+	        alert('fail')
+	      	}
+      	});
+  });
+}
