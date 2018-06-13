f79a2c16be079621a02ce8bd6b9704844cb17c9a
Yazan Halawa <yahalawa@microsoft.com>
2018-06-13 16:17:31 -0700

    Adding Drives depending on days

--- calendarIntegration.js
+++ calendarIntegration.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
-	var element = getTestDayElement()
-	get_drives("6qXoOAW6EeefM876KXeJ8A", "2017-11-25T00:00:00Z", "2017-11-27T23:59:59Z", function(response){
-  		alert(response[0]['appVersion']);
-  		for (var i = 0; i < response.length; i++){
-  			if (response[i]['createdAt'] > "2017-11-25T00:00:00Z" && response[i]['createdAt'] < "2017-11-25T23:59:59Z"){
-  			var drivesHTML = "<h2>Drives</h2>";
-			$(element).append(drivesHTML);
-  		}
-  		}
-  		
-	});
+	$( "div[role='main']" ).removeAttr('jsaction');
+	embedCardsInCalendar();
 });
 
-function getTestDayElement(){
+function embedCardsInCalendar(){
+	get_drives("6qXoOAW6EeefM876KXeJ8A", "2017-11-01T00:00:00Z", "2017-11-27T23:59:59Z", function(response){
+  		//alert(response[0]['appVersion']);
+  		for (var i = 0; i < response.length; i++){
+  			var drive = response[i];
+  			var driveCreationDate = new Date(drive.createdAt);
+  			var dayOfDrive = driveCreationDate.getDate();
+  			var dayElementOnCalendar = getTestDayElement(dayOfDrive);
+  			var drivesHTML = "<div class=\"calendarCard\" role=button>" +
+						            "<p><b>"+ drive.startLocCity +" -> "+ drive.endLocCity +"</b><br>"+
+						            "$"+ drive.potentialValues.business.default+"</p>"+
+					      	"</div>";
+			$(dayElementOnCalendar).append(drivesHTML);
+			$(dayElementOnCalendar).click(function(){ console.log(test)})
+			$(dayElementOnCalendar).children().click(function(){ console.log(test)})
+  		}
+	});
+}
+
+function getTestDayElement(day){
 	//this method grabs the title element of the 12th day of the current month
 	var h2Tags = $('h2');
 	for (var i = 0; i < h2Tags.length; i++){
-		if (h2Tags[i].innerHTML == "25"){
+		if (h2Tags[i].innerHTML == day){
 			return h2Tags[i].parentElement;
 		}
 	}
@@ -36,3 +46,10 @@ function getCurrentViewedDates(){
 	// in that date range
 }
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // listen for messages sent from background.js
+    if (request.message === 'shouldRerunCalendar') {
+    	embedCardsInCalendar();
+    }
+});
