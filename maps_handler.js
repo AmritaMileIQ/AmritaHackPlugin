02b419f0feafc1f0a6b2394c1061c8c5f579e9d2
Maha <magovin@microsoft.com>
2018-06-14 13:50:50 -0700

    UI changes

--- maps_handler.js
+++ maps_handler.js
@@ -12,10 +12,22 @@ function updateElements() {
      if ($(parent[i]).text().indexOf("miles") != -1) {
           var text =  $(parent[i]).text();
           var findString = "Leave around";
+          var index = text.indexOf(findString)
+          if(index == -1) {
+            findString = "min"
+          }
           var numOfMiles = text.substring(text.indexOf(findString) + findString.length, text.indexOf("miles")).trim()
           var cost = (numOfMiles * 0.535 ).toFixed(2)
-          var costHTML = "<h2>$" + cost +"</h2>";
+          var costString = ("$" + cost).fontcolor("green");
+          
+          var elem = document.createElement("img");
+          elem.setAttribute("src", chrome.extension.getURL("icon.png"));
+
+          var costHTML = "<strong>  " +costString +"</br> </strong>";
+          var potentialValueHTML = "<h2> POTENTIAL VALUE </h2>"
+          $(parent[i]).append(elem);
           $(parent[i]).append(costHTML);
+          $(parent[i]).append(potentialValueHTML);
       } 
    } 
 }
