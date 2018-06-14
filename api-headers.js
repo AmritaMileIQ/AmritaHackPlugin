4093f22e8ddcdffc690ee0e96c1a10d954f48b98
AmritaMileIQ <amchowdh@microsoft.com>
2018-06-13 17:19:26 -0700

    Add classify buttons to drive card for new tab

--- api-headers.js
+++ api-headers.js
@@ -2,7 +2,22 @@ function get_api_headers(user_parse_id) {
 	return {
           'X-MileIQ-Application-Id':'E7EC264F-F470-4C7C-95AF-4B5D08C57346',
           'X-MileIQ-API-Key':'1BD272D1-3DC4-4AF4-BCC6-D125C521788C',
-          'X-MileIQ-Acting-On': user_parse_id,
+          'X-MileIQ-Acting-On': '7vsH9G2eEeiszwpYCvQBsQ',
           'Content-Type':'application/json'
           }
 }
+
+function get_user_parse_id(){
+	return '7vsH9G2eEeiszwpYCvQBsQ'
+}
+
+var CLASSIFY_EXPENSE_UNCLASSIFIED = 0
+var CLASSIFY_EXPENSE_PERSONAL = 1
+var CLASSIFY_EXPENSE_BUSINESS = 2
+
+var CLASSIFY_DRIVE_UNCLASSIFIED = 0
+var CLASSIFY_DRIVE_PERSONAL = 1
+var CLASSIFY_DRIVE_BUSINESS = 2
+
+
+
--- mdl.js
+++ mdl.js
@@ -17,27 +17,29 @@ $(document).ready(function(){
 $(document).ready(function(){
 
     //fetch drives
-     	get_drives("6qXoOAW6EeefM876KXeJ8A", "2017-11-25T00:00:00Z", "2017-11-27T23:59:59Z", function(response){
+     	get_drives(get_user_parse_id(), "2018-01-01T00:00:00Z", "2018-07-01T23:59:59Z", function(response){
   			if ( response.length > 0 ){
-          for(var i=0;i<response.length;i++){
+          for(var i=0; i<response.length; i++){
             if ( i >= 5) {
               break;
             }
             createDriveInTab(response[i]);
           }
+          classifyDriveOnClickEventHandler() 
         }
 			});
 
       //fetch Expenses
-      get_expenses("bs-LolSWEeiSwL65ypsPBA", "2018-06-01", "2018-07-01", function(response){
+      get_expenses(get_user_parse_id(), "2015-01-01", "2018-07-01", function(response){
   			console.log(response);
         if ( response.results.length > 0 ){
-          for(var j=0;j<response.results.length;j++){
+          for(var j=0; j<response.results.length; j++){
              if ( j >= 5) {
               break;
              }
             createExpenseInTab(response.results[j]);
           }
+          classifyExpenseOnClickEventHandler()
         }
 			});
 	});
@@ -48,9 +50,26 @@ function createDriveInTab(drive) {
          "<div class=\"card-container\">"+
             "<h4><b>"+ drive.startLocCity +" -> "+ drive.endLocCity +"</b></h4>"+
             "<p>"+"Value $"+ drive.potentialValues.business.default+"</p>"+
+            "<input id=" + drive.objectId + " type=\"button\" value=\"Classify Business\" class=\"business-drive\"/>" +
+            "<input id=" + drive.objectId + " type=\"button\" value=\"Classify Personal\" class=\"personal-drive\"/>" +
          "</div>"
       "</div>";
+  
   $('.container').append(card);
+}
+
+function classifyDriveOnClickEventHandler() {
+  $(".business-drive").click(function() {
+          classify_drives(get_user_parse_id(), $(this).attr('id'), CLASSIFY_DRIVE_BUSINESS, function(response){
+            alert(response['code'])
+        });
+  });
+
+  $(".personal-drive").click(function() {
+          classify_drives(get_user_parse_id(), $(this).attr('id'), CLASSIFY_DRIVE_PERSONAL, function(response){
+            alert(response['code'])
+        });
+  });
 }
 
 
@@ -58,9 +77,27 @@ function createExpenseInTab(expense) {
   var card = "<div class=\"card\">" +
          "<img src=\"SpendIcon.png\" style=\"width:100%\">" +
          "<div class=\"card-container\">"+
-            "<h4><b>"+ expense.merchant_name_and_location +"</b></h4>"+
-            "<p>"+"Value $"+ expense.amount+"</p>"+
+            "<h4><b>"+ expense.merchant_name_and_location +"</b></h4>" +
+            "<p>"+"Value $"+ expense.amount+"</p>" +
+            "<input id=" + expense.id + " type=\"button\" value=\"Classify Business\" class=\"business-expense\"/>" +
+            "<input id=" + expense.id + " type=\"button\" value=\"Classify Personal\" class=\"personal-expense\"/>" +
          "</div>"
       "</div>";
   $('.container').append(card);
+}
+
+function classifyExpenseOnClickEventHandler() {
+  $(".business-expense").click(function() {
+          var expense_id = $(this).attr('id');
+          classify_expenses(get_user_parse_id(), CLASSIFY_EXPENSE_BUSINESS , expense_id, function(response){
+            alert(response['classification'])
+        });
+  });
+
+  $(".personal-expense").click(function() {
+         var expense_id = $(this).attr('id');
+         classify_expenses(get_user_parse_id(), CLASSIFY_EXPENSE_PERSONAL , expense_id, function(response){
+            alert(response['classification'])
+        });
+  });
 }
