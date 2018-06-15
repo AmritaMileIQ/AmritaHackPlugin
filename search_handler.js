cf3a2042a65c14cafdcc205d3d631705239c8516
Maha <magovin@microsoft.com>
2018-06-14 19:32:42 -0700

    MileIq Icon redirects to refer a friend page

--- search_handler.js
+++ search_handler.js
@@ -12,23 +12,24 @@ function updateElements() {
 	var searchText = parent.find("#lst-ib").val().toLowerCase().trim();
 	if(searchText.indexOf("turbotax") != -1) {
 		var results = $("#resultStats");
-		//console.log(results.text())
+
 		var divElement = document.createElement("div");
 		divElement.setAttribute("style", "margin-top:-25px;color:green");
 
-		 var imageElement = document.createElement("img");
-          imageElement.setAttribute("src", chrome.extension.getURL("icon_round.svg"));
-          // imageElement.setAttribute("hspace", "5px")
-         imageElement.setAttribute("style", "margin-top:10px;")
+		var imageElement = document.createElement("img");
+        imageElement.setAttribute("src", chrome.extension.getURL("icon_round.svg"));
+        imageElement.setAttribute("style", "margin-top:10px;")
+         
+        imageElement.onclick = function(event) {
+        window.location.href = "https://staging.dashboard.mileiq.com/settings/referral";
+  		  };
 
-         var pElement = document.createElement("p");
-         pElement.setAttribute("style", "margin-top:-47px; margin-left:25px;");
-
-
-          var textHTML = "Save $25 off Turbotax by referring a friend!"
-          pElement.append(textHTML);
-          divElement.append(imageElement);
-          divElement.append(pElement);
-          results.append(divElement);
+        var pElement = document.createElement("p");
+        pElement.setAttribute("style", "margin-top:-47px; margin-left:25px;");
+        var textHTML = "Save $25 off TurboTax by referring a friend!"
+        pElement.append(textHTML);
+        divElement.append(imageElement);
+        divElement.append(pElement);
+        results.append(divElement);
 	} 
 }
